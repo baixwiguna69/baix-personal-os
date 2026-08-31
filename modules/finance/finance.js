d7639cdbdeb16a230a3f86baf1c0057b9843b81f
baixwiguna69 <113879914+baixwiguna69@users.noreply.github.com>
2026-09-01 03:29:41 +0700

    Update today function to return formatted date
    Refactor today function to return formatted date string.

--- modules/finance/finance.js
+++ modules/finance/finance.js
@@ -70,14 +70,17 @@ function money(value) {
 
 
 function today() {
+    const date = new Date();
 
-    const date =
-        new Date();
+    const year = date.getFullYear();
+    const month = String(
+        date.getMonth() + 1
+    ).padStart(2, "0");
+    const day = String(
+        date.getDate()
+    ).padStart(2, "0");
 
-    return date
-        .toISOString()
-        .split("T")[0];
-
+    return `${year}-${month}-${day}`;
 }
 
 
